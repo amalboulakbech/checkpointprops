20dcbf0f1abf95773d3c577f17adda5c3acd2736
unknown <boulakbechamal@gmail.com>
2022-07-12 00:14:01 +0200

    first commit

---
+++ src/Data.js
@@ -0,0 +1,9 @@
+export const Data = [
+  {
+    fullName: "Boulakbech amal",
+    Bio: "Try Again",
+    Profession: "civil engineering",
+    image:
+      "https://www.genie-inc.com/uploads/public/gi/article/81360__woman.jpg",
+  },
+];
